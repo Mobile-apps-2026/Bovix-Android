55562b26b809824be35ebe5255fecfe0d6321f5f
U202213553 <u202213553@upc.edu.pe>
2026-06-21 23:12:14 -0500

    Firebase correction

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,6 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
-    id("com.android.application")
-    id("com.google.gms.google-services")
 }
 
 android {
@@ -93,6 +91,4 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
     //Firebase
-    implementation(platform("com.google.firebase:firebase-bom:34.15.0"))
-    implementation("com.google.firebase:firebase-analytics")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,4 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
     // Add the dependency for the Google services Gradle plugin
-    id("com.google.gms.google-services") version "4.5.0" apply false
-
 }
